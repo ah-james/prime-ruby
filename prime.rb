9e0ba69ee6d5fb72382de78ed2b07a200ee4f6aa
ah-james <11ahj11@gmail.com>
2020-10-26 13:43:02 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(number)
+    if number <= 1
+        return false
+    else
+        (2..number-1).to_a.all? do |prime|
+            number % prime != 0
+        end
+    end
+end
